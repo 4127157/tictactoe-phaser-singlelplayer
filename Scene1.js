1ca522e687a38084b45bc1d56055ae4501b1641e
Aryansh <89254856+4127157@users.noreply.github.com>
2022-07-28 14:44:13 +0530

    loading assets in scene1

--- Scene1.js
+++ Scene1.js
@@ -1,10 +1,13 @@
 class Scene1 extends Phaser.Scene {
     constructor(){
-        super("startGame");
+        super("menuScreen");
     }
 
 
     preload(){
+        this.load.image('cross', './assets/Sprite-0001_512.png');
+        this.load.image('circle', './assets/Sprite-0002_512.png');
+        this.load.image('board', './assets/board_512.png');
     }
 
     create(){
--- game.js
+++ game.js
@@ -1,12 +1,14 @@
-var config = {
+var gameConfig = {
     type:Phaser.AUTO,
     scale: {
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width:495,
         height: 880
     },
+    backgroundColor: 'rgba(255,255,255,0)',
+    transparent:false,
     scene: [Scene1],
     pixelArt: true
 };
 
-var game = new Phaser.Game(config);
+var game = new Phaser.Game(gameConfig);
